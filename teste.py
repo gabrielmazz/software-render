c936e2ab2395d189aa6045ae7c2601c57c12de0a
Gabriel Mazzuco <gabrielalvesmazzuco@hotmail.com>
2024-05-31 11:13:27 -0300

    Correções e adição de cor no objeto

--- teste.py
+++
@@ -1,7 +0,0 @@
-def hex_to_rgb(hex_color):
-    hex_color = hex_color.lstrip('#')
-    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-
-# Testando a função
-rgb_color = hex_to_rgb("#e346ff")
-print(rgb_color)
